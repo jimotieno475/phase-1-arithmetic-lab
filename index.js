d7335dfa02e34c4809296e85907c8140d73fc912
jim <jim.otieno@student.moringaschool.com>
2023-09-18 23:11:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+let num1 = 31;
+let num2 =2;
+let multiply = num1 * num2 ;
+Math.floor(Math.random() * 10) + 1;
+let random =1
+Math.floor(Math.random() * 10) + 1;
+let num3 =14;
+let num4 =5
+let mod = (14 % 5);
+console.log(14 % 5);
+// Expected output: 4
+let max =20;
+Math.max([10, 2,20]);
